761d391c6bd05835a333799f8c0159a1df1af135
chaiyuze <chaiyuze@dpstorm.com>
2019-05-30 00:59:15 +0800

    Desc: 补充浮点数native方法 Type: add Ver: Func: Author: chaiyuze

---
+++ ch09/native/java/lang/double.go
@@ -0,0 +1,24 @@
+package lang
+
+import (
+	"jvmgo/ch09/native"
+	"jvmgo/ch09/rtda"
+	"math"
+)
+
+func init() {
+	native.Register("java/lang/Double", "doubleToRawLongBits", "(D)J", doubleToRawLongBits)
+	native.Register("java/lang/Double", "longBitsToDouble", "(J)D", longBitsToDouble)
+}
+
+func doubleToRawLongBits(frame *rtda.Frame) {
+	value := frame.LocalVars().GetDouble(0)
+	bits := math.Float64bits(value)
+	frame.OpStack().PushLong(int64(bits))
+}
+
+func longBitsToDouble(frame *rtda.Frame) {
+	bits := frame.LocalVars().GetLong(0)
+	value := math.Float64frombits(uint64(bits))
+	frame.OpStack().PushDouble(value)
+}
